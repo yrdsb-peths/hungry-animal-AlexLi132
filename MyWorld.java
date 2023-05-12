5287941382e09c3d7c9a9f74495f8b3a85bf8224
AlexLi132 <130993289+AlexLi132@users.noreply.github.com>
2023-05-12 09:03:44 -0400

    Added Elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * The world our hero resides in.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Alex
+ * @version May 12th 
  */
 public class MyWorld extends World
 {

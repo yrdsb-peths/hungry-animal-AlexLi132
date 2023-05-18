b120c5945adfc1f08bdc27177526c51998f71af5
AlexLi132 <130993289+AlexLi132@users.noreply.github.com>
2023-05-18 09:00:40 -0400

    Added game effect sound when eating an apple

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("gameding.mp3");
     public void act()
     {
         // Add your action code here.
@@ -34,10 +31,12 @@ public class Elephant extends Actor
     {
         if(isTouching(Apple.class))
         {
+            elephantSound.play();
             removeTouching(Apple.class);
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            
 
         }
     }
